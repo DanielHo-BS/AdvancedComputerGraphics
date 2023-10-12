696e0419cf5874a5691c9862456b929d11e5eeb6
DanielHo-BS <danielho1671@gmail.com>
2023-10-12 21:48:29 +0800

    Add HW2

---
+++ HW2/README.md
@@ -0,0 +1,92 @@
+# HW2
+
+ Write a program to do an animated ``clock``.
+
+- Javascript
+- HTML
+- Visual Studio Code with ``live server``
+
+## Description
+
+1. Write or modify a program to show an animation with rotating hour and minute
+hands at right positions.
+2. In this assignment, please create a circle to present the body of a clock whose center us at (100, 50, 0). And, create extra two hands, (as a shape of diamond or triangle), and their rotation centers align with the body of clock. Finally, create a rotation animation.
+Hint: the speed of minute hand is faster (12x) than hour hand’s.
+
+## Install
+
+Environment:
+
+- WSL
+- Ubuntu 22.04
+- Visual Studio Code
+
+Install:
+
+- nvm
+- Node.js
+- npm
+
+### nvm
+
+Install ``nvm`` in the Ubuntu
+
+```bash
+sudo apt-get install curl
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+```
+
+Resource the bash
+
+```bash
+source ~/.bashrc
+```
+
+Check nvm
+
+```bash
+command -v nvm
+
+nvm ls
+```
+
+### Node.js
+
+Install the **LTS** version
+
+```bash
+nvm install --lts
+```
+
+Check version of Node.js
+
+```bash
+nvm ls
+
+node --version
+```
+
+### npm
+
+Check the npm
+
+```bash
+npm -v
+```
+
+Install the JS library ``three``
+
+```bash
+npm install three
+```
+
+## Result
+
+Watch the video
+
+[![Watch the video](./clock.png)](https://youtu.be/bM1XUi3g194)
+
+## Reference
+
+[在 Windows 子系統 Linux 版 (WSL2) 上安裝 Node.js](https://learn.microsoft.com/zh-tw/windows/dev-environment/javascript/nodejs-on-wsl)
---
+++ HW2/main.js
@@ -0,0 +1,142 @@
+var render
+var scene
+var camera
+
+function animateFrame() {
+    //Get the object from scene
+    var centerOfRotation = new THREE.Vector3(100, 50, 0);
+    const rotationAxis = new THREE.Vector3(0, 0, 1);
+    const rotationSpeed = -2.5 / 180 * Math.PI; // 2.5 degree per second
+    
+    var hour = scene.getObjectByName('hour')
+    if (hour) {
+        Rotation(hour, rotationAxis, rotationSpeed / 12, centerOfRotation);
+    }
+    var minute = scene.getObjectByName('minute')
+    if (minute) {
+        Rotation(minute, rotationAxis, rotationSpeed, centerOfRotation);
+    }
+
+    render.render(scene, camera);
+    requestAnimationFrame(animateFrame)
+}
+
+function Rotation(mesh, rotationAxis, rotationSpeed, centerOfRotation) {
+    // Set the center of rotation
+    mesh.position.sub(centerOfRotation);
+    var rotationMatrix  = new THREE.Matrix4().makeRotationAxis(rotationAxis, rotationSpeed);
+    mesh.applyMatrix4(rotationMatrix);
+    mesh.position.add(centerOfRotation);
+}
+
+function Dodecagon(centerX, centerY, centerZ, radius, numSides) {
+    //Dodecagon (as vertices)
+    var angle = (2 * Math.PI) / numSides;
+    var vertices = [];
+    vertices.push(new THREE.Vector3(centerX, centerY, centerZ));  // center of the circle
+    for (let i = 0; i < numSides; i++) {
+        const x = centerX + radius * Math.cos(i * angle);
+        const y = centerY + radius * Math.sin(i * angle);
+        const z = centerZ; // Z-coordinate is set to 0
+
+        vertices.push(new THREE.Vector3(x, y, z));
+    }
+
+    vertices
+
+    //Dodecagon (as indices)
+    var indices = [
+        0, 1, 2,
+        2, 3, 0,
+        0, 3, 4,
+        4, 5, 0,
+        0, 5, 6,
+        6, 7, 0,
+        0, 7, 8,
+        8, 9, 0,
+        0, 9, 10,
+        10, 11, 0,
+        0, 11, 12,
+        12, 1, 0
+    ];
+
+    //geometry
+    var geometry = new THREE.BufferGeometry().setFromPoints(vertices);
+    geometry.setIndex(indices);
+
+    //material 
+    var material = new THREE.MeshBasicMaterial({ color: 0x333333, wireframe: false });
+
+    //Mesh 
+    return new THREE.Mesh(geometry, material);
+}
+
+function Diamond(centerX, centerY, centerZ, radius, color) {
+    var vertices = [];
+    vertices.push(new THREE.Vector3(centerX, centerY, centerZ));  //0
+    vertices.push(new THREE.Vector3(centerX, centerY - 25, centerZ)); //1
+    vertices.push(new THREE.Vector3(centerX + 50 * radius, centerY - 50 * radius, centerZ)); //2
+    vertices.push(new THREE.Vector3(centerX + 25, centerY, centerZ)); //3
+
+    //Diamond (as indices)
+    var indices = [
+        0, 1, 2,
+        2, 3, 0,
+    ];
+
+    //geometry
+    var geometry = new THREE.BufferGeometry().setFromPoints(vertices);
+    geometry.setIndex(indices);
+
+    //material 
+    var material = new THREE.MeshBasicMaterial({ color: color, wireframe: false });
+
+    //Mesh 
+    return new THREE.Mesh(geometry, material);
+}
+
+function main() {
+    //camera (as globle var)
+    camera = new THREE.OrthographicCamera(640 / - 2, 640 / 2, 480 / 2, 480 / - 2, -1000, 1000);
+    camera.position.set(0, 0, 5);
+
+    //Create Mesh 
+    var mesh = Dodecagon(0, 0, 0, 150, 12);
+    mesh.name = 'circle'
+    var mesh2 = Diamond(0, 0, 2, 1, 0x004B97);
+    mesh2.name = 'hour'
+    var mesh3 = Diamond(0, 0, 1, 2, 0x0080FF);
+    mesh3.name = 'minute'
+
+
+    //Set the position of the object
+    mesh.position.set(100, 50, 0)
+    mesh2.position.set(100, 50, 0)
+    mesh3.position.set(100, 50, 0)
+
+    //Set rotation >> 3:00
+    mesh2.rotateZ(45 / 180 * Math.PI);
+    mesh3.rotateZ(135 / 180 * Math.PI);
+
+    var gridHelper = new THREE.GridHelper(500, 10);
+    gridHelper.geometry.rotateX(- Math.PI / 2);
+
+    //Scene (as globle var)
+    scene = new THREE.Scene();
+    scene.add(camera);
+    scene.add(mesh);
+    scene.add(mesh2);
+    scene.add(mesh3);
+    scene.add(gridHelper);
+
+    //Render (as globle var)
+    render = new THREE.WebGLRenderer();
+    render.setClearColor(0x000000, 1);
+    render.setSize(640, 480);
+    document.body.appendChild(render.domElement);
+
+    animateFrame();
+}
+
+
+main();
